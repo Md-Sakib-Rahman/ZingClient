10fb808d86ad3470ecb08d3338b44ca51a9aa4dd
Md Sakib Rahman <sakibrahman.user@gmail.com>
2026-01-29 21:29:02 +0600

    removed newsletter

--- src/Components/Shared/Footer/Footer.jsx
+++ src/Components/Shared/Footer/Footer.jsx
@@ -23,20 +23,7 @@ const Footer = () => {
         <a className="link link-hover">Privacy policy</a>
         <a className="link link-hover">Cookie policy</a>
       </nav>
-      <form>
-        <h6 className="footer-title">Newsletter</h6>
-        <fieldset className="w-80">
-          <label>Enter your email address</label>
-          <div className="join">
-            <input
-              type="text"
-              placeholder="username@site.com"
-              className="input input-bordered join-item w-full"
-            />
-            <button className="btn btn-primary join-item">Subscribe</button>
-          </div>
-        </fieldset>
-      </form>
+      
     </footer>
   );
 };
